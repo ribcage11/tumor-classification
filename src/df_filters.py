afbb2c19454ed9e86a1323acc8559b7cb5b81845
Jia Yi Lu <jlu357x@proton.me>
2024-05-31 15:19:15 -0700

    Added df_filters as wrapper functions for df filtering

--- src/df_filters.py
+++ src/df_filters.py
@@ -1,9 +1,25 @@
 import pandas as pd
+import xarray as xr
+
 
 # filter data based on column
-
 def filter_data(df, col, value):
     result = df[df[col] == value]
     return result
 
-def
+
+# get all data given patient
+def get_patient(scans_df, pid):
+    patient = scans_df[scans_df['BraTS21ID'] == pid]
+    return patient
+
+
+# get specific file path from image_id in patient
+def get_image_path(scans_df, pid, im_id):
+    patient = get_patient(scans_df, pid)
+    image = patient[patient['image_id'] == im_id]
+    return image
+
+
+
+
